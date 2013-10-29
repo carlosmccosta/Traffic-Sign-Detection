a168167a045c6dd95a1d49164480538cd4d18727
Carlos Miguel Correia da Costa <carloscosta.cmcc@gmail.com>
2013-10-29 07:25:00 +0000

    Image preprocessing (histogram equalization, bilateral filter, contrast, brightness).

--- Proj1-TrafficSignDetection/src/ImageAnalysis/ImageAnalysis.cpp
+++ Proj1-TrafficSignDetection/src/ImageAnalysis/ImageAnalysis.cpp
@@ -1,5 +1,61 @@
 #include "ImageAnalysis.h"
 
+
+
+bool ImageAnalysis::processImage(Mat& image, bool useCVHighGUI) {
+	detectedSigns.clear();
+	originalImage = image.clone();
+
+	if (useCVHighGUI) {
+		setupMainWindow();
+		imshow(WINDOW_NAME_MAIN, originalImage);
+		setupResultsWindows();
+	}
+
+	preprocessImage(image, useCVHighGUI);
+
+
+	if (useCVHighGUI) {
+		outputResults();		
+	}
+
+	return true;
+}
+
+
+
+void ImageAnalysis::preprocessImage(Mat& image, bool useCVHighGUI ) {	
+	// histogram equalization to improve color segmentation
+	cvtColor(image, image, CV_BGR2YCrCb);
+	vector<Mat> channels;
+	cv::split(image, channels);
+	cv::equalizeHist(channels[0], channels[0]);
+	cv::merge(channels, image);
+	cvtColor(image, image, CV_YCrCb2BGR);
+	imshow(WINDOW_NAME_HISTOGRAM_EQUALIZATION, image);
+
+	// remove noise with bilateral filter
+	cv::bilateralFilter(originalImage, image, 9, 50, 10);		
+	imshow(WINDOW_NAME_BILATERAL_FILTER, image);
+
+	// increase contrast and brightness to improve detection of numbers inside traffic sign
+	image.convertTo(image, -1, 1.5, 20);
+	imshow(WINDOW_NAME_CONTRAST_AND_BRIGHTNESS, image);	
+}
+
+void ImageAnalysis::segmentImage(Mat& image, bool useCVHighGUI) {
+
+}
+
+void ImageAnalysis::recognizeTrafficSigns(Mat& image, bool useCVHighGUI) {
+
+}
+
+
+
+
+
+
 bool ImageAnalysis::processImage(string path, bool useCVHighGUI) {		
 	Mat imageToProcess;
 	if (path != "") {
@@ -16,30 +72,13 @@ bool ImageAnalysis::processImage(string path, bool useCVHighGUI) {
 		return false;
 	}
 	
-	return processImage(imageToProcess, useCVHighGUI);	
-}
+	bool status = processImage(imageToProcess, useCVHighGUI);	
 
-
-bool ImageAnalysis::processImage(Mat image, bool useCVHighGUI) {
-	originalImage = image;
-	
-	if (useCVHighGUI) {
-		setupMainWindow();
-		imshow(NAME_MAIN_WINDOW, originalImage);		
+	if (waitKey(0) == ESC_KEYCODE) {
+		cv::destroyAllWindows();
 	}
 
-	detectedSigns.clear();
-
-
-	if (useCVHighGUI) {
-		outputResults();
-
-		if (waitKey(0) == ESC_KEYCODE) {
-			cv::destroyAllWindows();
-		}
-	}
-
-	return true;
+	return status;
 }
 
 
@@ -47,7 +86,7 @@ bool ImageAnalysis::processVideo(string path, bool useCVHighGUI) {
 	VideoCapture videoCapture;
 	
 	try {
-		videoCapture.open(path);
+		videoCapture = VideoCapture(path);
 	} catch (...) {
 		return false;
 	}
@@ -60,7 +99,7 @@ bool ImageAnalysis::processVideo(int cameraDeviceNumber, bool useCVHighGUI) {
 	VideoCapture videoCapture;
 
 	try {
-		videoCapture.open(cameraDeviceNumber);
+		videoCapture = VideoCapture(cameraDeviceNumber);
 	} catch (...) {
 		return false;
 	}
@@ -74,20 +113,15 @@ bool ImageAnalysis::processVideo(VideoCapture videoCapture, bool useCVHighGUI) {
 		return false;
 	}
 
-	if (useCVHighGUI) {
-		setupMainWindow();
-	}
-
 	int millisecPollingInterval = 1000 / frameRate;
+	if (millisecPollingInterval < 10)
+		millisecPollingInterval = 10;
 	
-	while (videoCapture.read(originalImage)) {
-		processImage(originalImage, false);
+	Mat currentFrame;
 
-		if (useCVHighGUI) {
-			imshow(NAME_MAIN_WINDOW, originalImage);
-			outputResults();
-		}
-
+	while (videoCapture.read(currentFrame)) {
+		processImage(currentFrame, useCVHighGUI);
+		
 		if (waitKey(millisecPollingInterval) == ESC_KEYCODE) {
 			break;
 		}
@@ -101,18 +135,48 @@ bool ImageAnalysis::processVideo(VideoCapture videoCapture, bool useCVHighGUI) {
 }
 
 
-void ImageAnalysis::setupMainWindow() {
-	namedWindow(NAME_MAIN_WINDOW, CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_EXPANDED);	
-	moveWindow(NAME_MAIN_WINDOW, 0, 0);
+void ImageAnalysis::setupMainWindow() {	
+	addWindow(0, 0, WINDOW_NAME_MAIN);
 }
 
 
 void ImageAnalysis::setupResultsWindows() {
-	
+	addWindow(1, 0, WINDOW_NAME_HISTOGRAM_EQUALIZATION);
+	addWindow(2, 0, WINDOW_NAME_BILATERAL_FILTER);
+	addWindow(3, 0, WINDOW_NAME_CONTRAST_AND_BRIGHTNESS);	
 }
+
+
+void ImageAnalysis::addWindow(int column, int row, string name, int numberColumns, int numberRows) {
+	namedWindow(name, CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_EXPANDED);	
+	
+	int height = screenHeight / numberRows;
+	int width = originalImage.size().width * height / originalImage.size().height;
+
+	if (width * numberColumns > screenWidth) {		
+		width = (screenWidth / numberColumns) - 2 * WINDOW_FRAME_THICKNESS;
+		height = originalImage.size().height * width / originalImage.size().width;
+	}
+
+	resizeWindow(name, width, height);
+	
+	int x = 0;
+	if (column != 0) {
+		x = (width + WINDOW_FRAME_THICKNESS * 2) * column;
+	}
+
+	int y = 0;
+	if (row != 0) {
+		y = (height + WINDOW_HEADER_HEIGHT + WINDOW_FRAME_THICKNESS) * row;
+	}
+
+	moveWindow(name, x, y);
+}
+
 
 bool ImageAnalysis::outputResults() {
 	setupResultsWindows();
 
 	return true;
 }
+
--- Proj1-TrafficSignDetection/src/ImageAnalysis/ImageAnalysis.h
+++ Proj1-TrafficSignDetection/src/ImageAnalysis/ImageAnalysis.h
@@ -15,9 +15,17 @@ using cv::waitKey;
 using cv::imshow;
 using cv::namedWindow;
 using cv::moveWindow;
+using cv::resizeWindow;
 
 
-#define NAME_MAIN_WINDOW "Image source"
+#define WINDOW_NAME_MAIN "0. Original image"
+#define WINDOW_NAME_HISTOGRAM_EQUALIZATION "1. Histogram equalization"
+#define WINDOW_NAME_BILATERAL_FILTER "2. Bilateral filter"
+#define WINDOW_NAME_CONTRAST_AND_BRIGHTNESS "3. Contrast and brightness"
+
+
+#define	WINDOW_HEADER_HEIGHT 32
+#define WINDOW_FRAME_THICKNESS 8
 #define ESC_KEYCODE 27
 
 class ImageAnalysis {
@@ -26,14 +34,16 @@ class ImageAnalysis {
 		Mat originalImage;
 
 		int frameRate;
+		int screenWidth;
+		int screenHeight;
 
 
 	public:
-		ImageAnalysis() : frameRate(30) {};
+		ImageAnalysis() : frameRate(30), screenWidth(1920), screenHeight(1080) {};
 		virtual ~ImageAnalysis() {}
 		
 		bool processImage(string path, bool useCVHighGUI = true);
-		bool processImage(Mat image, bool useCVHighGUI = true);	
+		bool processImage(Mat& image, bool useCVHighGUI = true);	
 		
 		bool processVideo(string path, bool useCVHighGUI = true);
 		bool processVideo(int cameraDeviceNumber, bool useCVHighGUI = true);
@@ -42,5 +52,13 @@ class ImageAnalysis {
 		void setupMainWindow();
 		void setupResultsWindows();
 		bool outputResults();
+
+
+		void preprocessImage(Mat& image, bool useCVHighGUI = true);
+		void segmentImage(Mat& image, bool useCVHighGUI = true);
+		void recognizeTrafficSigns(Mat& image, bool useCVHighGUI = true);
+
+		void addWindow(int column, int row, string name, int numberColumns = 4, int numberRows = 2);
+
 };
 
