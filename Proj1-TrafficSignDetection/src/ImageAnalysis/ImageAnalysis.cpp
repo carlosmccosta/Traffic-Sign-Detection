30304e189311b0d3af59ccd4d6f84803f3cad5ff
Carlos Miguel Correia da Costa <carloscosta.cmcc@gmail.com>
2013-10-31 23:54:32 +0000

    Added detection of signs using HoughCircles transform.

--- Proj1-TrafficSignDetection/src/ImageAnalysis/ImageAnalysis.cpp
+++ Proj1-TrafficSignDetection/src/ImageAnalysis/ImageAnalysis.cpp
@@ -7,10 +7,14 @@ ImageAnalysis::ImageAnalysis() :
 	frameRate(30), screenWidth(1920), screenHeight(1080),
 	claehClipLimit(2), claehTileXSize(2), claehTileYSize(2),
 	bilateralFilterDistance(9), bilateralFilterSigmaColor(50), bilateralFilterSigmaSpace(10),
-	contrast(12), brightness(50),
+	contrast(11), brightness(25),
 	colorSegmentationLowerHue(145), colorSegmentationUpperHue(10),
 	colorSegmentationLowerSaturation(56), colorSegmentationUpperSaturation(255),
-	colorSegmentationLowerValue(64), colorSegmentationUpperValue(255) {};
+	colorSegmentationLowerValue(64), colorSegmentationUpperValue(255),
+	cannyLowerHysteresisThreshold(100), cannyHigherHysteresisThreshold(200), cannySobelOperatorKernelSize(3),
+	houghCirclesDP(1), houghCirclesMinDistanceCenters(4),
+	houghCirclesCannyHigherThreshold(200), houghCirclesAccumulatorThreshold(25),
+	houghCirclesMinRadius(2), houghCirclesMaxRadius(100) {};
 
 
 ImageAnalysis::~ImageAnalysis() {
@@ -60,21 +64,20 @@ bool ImageAnalysis::processImage(Mat& image, bool useCVHighGUI) {
 			setupMainWindow();
 			setupResultsWindows();
 			windowsInitialized = true;
-		}		
-
-		imshow(WINDOW_NAME_MAIN, originalImage);		
+		}				
 	}
 
 	preprocessedImage = image.clone();
 	preprocessImage(preprocessedImage, useCVHighGUI);
 
-	Mat imageColorSegmented = preprocessedImage.clone();
-	segmentImage(preprocessedImage, useCVHighGUI);
+	Mat imageColorSegmented = segmentImageByColor(preprocessedImage, useCVHighGUI);
+	recognizeTrafficSignsCircles(imageColorSegmented, preprocessedImage, useCVHighGUI);
 	
-
+	processedImage = preprocessedImage;
 	if (useCVHighGUI) {
-		//imshow(WINDOW_NAME_SIGNAL_RECOGNITION, processedImage);
-		outputResults();		
+		imshow(WINDOW_NAME_MAIN, originalImage);
+		imshow(WINDOW_NAME_SIGNAL_RECOGNITION, processedImage);
+		outputResults();
 	}
 
 	return true;
@@ -89,7 +92,7 @@ void ImageAnalysis::preprocessImage(Mat& image, bool useCVHighGUI ) {
 	}
 
 	// histogram equalization to improve color segmentation
-	histogramEqualization(image.clone(), false, useCVHighGUI);
+	//histogramEqualization(image.clone(), false, useCVHighGUI);
 	histogramEqualization(image, true, useCVHighGUI);
 
 	// increase contrast and brightness to improve detection of numbers inside traffic sign
@@ -124,7 +127,7 @@ void ImageAnalysis::histogramEqualization(Mat& image, bool use_CLAHE, bool useCV
 }
 
 
-void ImageAnalysis::segmentImage(Mat& image, bool useCVHighGUI) {
+Mat ImageAnalysis::segmentImageByColor(Mat& image, bool useCVHighGUI) {
 	// color segmentation	
 	cvtColor(image, image, CV_BGR2HSV);
 	Mat colorSegmentation;
@@ -155,12 +158,159 @@ void ImageAnalysis::segmentImage(Mat& image, bool useCVHighGUI) {
 	if (useCVHighGUI) {
 		imshow(WINDOW_NAME_COLOR_SEGMENTATION, colorSegmentation);
 	}
+
+	return colorSegmentation;
 }
 
 
-void ImageAnalysis::recognizeTrafficSigns(Mat& image, Mat colorSegmentedImage, bool useCVHighGUI) {
+vector<Vec3f> ImageAnalysis::recognizeTrafficSignsCircles(Mat& colorSegmentedImage, Mat& image, bool useCVHighGUI) {	
+	/*if (cannySobelOperatorKernelSize == 4 || cannySobelOperatorKernelSize == 6)
+		--cannySobelOperatorKernelSize;
 
+	if (cannySobelOperatorKernelSize < 3)
+		cannySobelOperatorKernelSize = 3;
+
+	if (cannySobelOperatorKernelSize > 7)
+		cannySobelOperatorKernelSize = 7;
+
+	Canny(colorSegmentedImage, colorSegmentedImage,
+		(cannyLowerHysteresisThreshold < 1 ? 1 : cannyLowerHysteresisThreshold),
+		(cannyHigherHysteresisThreshold < 1 ? 1 : cannyHigherHysteresisThreshold),
+		cannySobelOperatorKernelSize, true);
+	*/
+	/*if (useCVHighGUI) {
+		imshow(WINDOW_NAME_SIGNAL_CANNY, colorSegmentedImage);
+	}*/
+
+	vector<Vec3f> houghCircles;
+	int imageHeight = colorSegmentedImage.size().height;
+	int circlesMinDistanceCenters = imageHeight * houghCirclesMinDistanceCenters / 100;
+	int circlesMinRadius = imageHeight * houghCirclesMinRadius / 100;
+	int circlesMaxRadius = imageHeight * houghCirclesMaxRadius / 100;
+
+	cv::HoughCircles(colorSegmentedImage, houghCircles, CV_HOUGH_GRADIENT,
+		(houghCirclesDP < 1 ? 1 : houghCirclesDP),
+		(circlesMinDistanceCenters < 1 ? 1 : circlesMinDistanceCenters),
+		(houghCirclesCannyHigherThreshold < 1 ? 1 : houghCirclesCannyHigherThreshold),
+		(houghCirclesAccumulatorThreshold < 1 ? 1 : houghCirclesAccumulatorThreshold),
+		circlesMinRadius, circlesMaxRadius);
+	
+	vector<Vec3f> houghCirclesFiltered = filterRecognizedTrafficSignCircles(houghCircles);	
+
+	if (useCVHighGUI) {
+		for (size_t i = 0; i < houghCirclesFiltered.size(); ++i) {
+			Point center(cvRound(houghCirclesFiltered[i][0]), cvRound(houghCirclesFiltered[i][1]));
+			int radius = cvRound(houghCirclesFiltered[i][2]);
+
+			circle(image, center, 1, Scalar(255,0,0), 2, 8, 0);
+			circle(image, center, radius, Scalar(0,255,0), 2, 8, 0);
+		}
+	}
+
+	return houghCirclesFiltered;
 }
+
+
+bool sortCircleClusterByMedianY(const Vec3f& left, const Vec3f& right) {
+	return left[1] < right[1];
+}
+
+bool sortCircleClusterByRadius(const Vec3f& left, const Vec3f& right) {
+	return left[2] < right[2];
+}
+
+vector<Vec3f> ImageAnalysis::filterRecognizedTrafficSignCircles(const vector<Vec3f>& houghCircles) {
+	if (houghCircles.size() < 2) {
+		return houghCircles;
+	}
+	
+	// aggregate circles closer to each other (with centers inside each other forming a cluster)
+	vector< vector<Vec3f> > houghCirclesClusters;
+	vector<Vec3f> firstCluster;
+	firstCluster.push_back(houghCircles[0]);
+	houghCirclesClusters.push_back(firstCluster);
+
+	for (size_t allCirclesPos = 1; allCirclesPos < houghCircles.size(); ++allCirclesPos) {
+		const Vec3f& centerToAdd = houghCircles[allCirclesPos];
+		
+		if (!aggregateCircleIntoClusters(houghCirclesClusters, centerToAdd)) {
+			vector<Vec3f> newCluster;
+			newCluster.push_back(centerToAdd);
+			houghCirclesClusters.push_back(newCluster);
+		}
+	}
+
+
+	// select the circle with the median y in each cluster (different traffic sign)
+	vector<Vec3f> houghCirclesFiltered;
+	flatClustersByMedian(houghCirclesClusters, houghCirclesFiltered);
+	//flatClustersByMaxRadius(houghCirclesClusters, houghCirclesFiltered);
+
+	return houghCirclesFiltered;
+}
+
+
+void ImageAnalysis::flatClustersByMedian(vector< vector<Vec3f> > &houghCirclesClusters, vector<Vec3f> &houghCirclesFiltered) {	
+	for (size_t circleClusterPos = 0; circleClusterPos < houghCirclesClusters.size(); ++circleClusterPos) {
+		vector<Vec3f> currentCluster = houghCirclesClusters[circleClusterPos];
+
+		if (currentCluster.size() > 1) {
+			sort(currentCluster.begin(), currentCluster.end(), sortCircleClusterByMedianY);
+			Vec3f selectedCircle;
+			int middlePos = currentCluster.size() / 2;
+			int middlePosMinus1 = middlePos - 1;
+			if (currentCluster.size() % 2) {	
+				selectedCircle[0] = (currentCluster[middlePos][0] + currentCluster[middlePosMinus1][0]) / 2;
+				selectedCircle[1] = (currentCluster[middlePos][1] + currentCluster[middlePosMinus1][1]) / 2;
+				selectedCircle[2] = (currentCluster[middlePos][2] + currentCluster[middlePosMinus1][2]) / 2;
+			} else {
+				selectedCircle = currentCluster[middlePos];
+			}
+			houghCirclesFiltered.push_back(selectedCircle);
+		} else if (currentCluster.size() == 1) {
+			houghCirclesFiltered.push_back(currentCluster[0]);
+		}
+	}
+}
+
+
+void ImageAnalysis::flatClustersByMaxRadius(vector< vector<Vec3f> > &houghCirclesClusters, vector<Vec3f> &houghCirclesFiltered) {	
+	for (size_t circleClusterPos = 0; circleClusterPos < houghCirclesClusters.size(); ++circleClusterPos) {
+		vector<Vec3f> currentCluster = houghCirclesClusters[circleClusterPos];
+
+		if (currentCluster.size() > 1) {
+			sort(currentCluster.begin(), currentCluster.end(), sortCircleClusterByRadius);			
+			houghCirclesFiltered.push_back(currentCluster.back());
+		} else if (currentCluster.size() == 1) {
+			houghCirclesFiltered.push_back(currentCluster[0]);
+		}
+	}
+}
+
+
+
+
+bool ImageAnalysis::aggregateCircleIntoClusters(vector< vector<Vec3f> >& houghCirclesClusters, const Vec3f& centerToAdd) {
+	for (size_t clusterPos = 0; clusterPos < houghCirclesClusters.size(); ++clusterPos) {
+		size_t numberCirclesToTest = houghCirclesClusters[clusterPos].size();
+		for (size_t centerToTestPos = 0; centerToTestPos < numberCirclesToTest; ++centerToTestPos) {
+			const Vec3f& centerToTest = houghCirclesClusters[clusterPos][centerToTestPos];
+			float maxRadius = std::max(centerToTest[2], centerToAdd[2]);
+			float dx = centerToTest[0] - centerToAdd[0];
+			float dy = centerToTest[1] - centerToAdd[1];
+			float distance = std::sqrt(dx*dx + dy*dy);
+
+			// aggregate the new circle if one of the then has the center inside of the other
+			if (distance < maxRadius) {
+				houghCirclesClusters[clusterPos].push_back(centerToAdd);				
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
+
 
 
 bool ImageAnalysis::updateImage() {
@@ -242,31 +392,35 @@ void ImageAnalysis::setupMainWindow() {
 
 void ImageAnalysis::setupResultsWindows(bool optionsOneWindow) {
 	addHighGUIWindow(1, 0, WINDOW_NAME_BILATERAL_FILTER);
-	addHighGUIWindow(2, 0, WINDOW_NAME_HISTOGRAM_EQUALIZATION);
-	addHighGUIWindow(0, 1, WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE);
-	addHighGUIWindow(1, 1, WINDOW_NAME_CONTRAST_AND_BRIGHTNESS);
-	addHighGUIWindow(2, 1, WINDOW_NAME_COLOR_SEGMENTATION);
-	//addHighGUIWindow(2, 1, WINDOW_NAME_SIGNAL_RECOGNITION);
+	//addHighGUIWindow(2, 0, WINDOW_NAME_HISTOGRAM_EQUALIZATION);
+	addHighGUIWindow(2, 0, WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE);
+	addHighGUIWindow(0, 1, WINDOW_NAME_CONTRAST_AND_BRIGHTNESS);
+	addHighGUIWindow(1, 1, WINDOW_NAME_COLOR_SEGMENTATION);
+	//addHighGUIWindow(1, 1, WINDOW_NAME_SIGNAL_CANNY);
+	addHighGUIWindow(2, 1, WINDOW_NAME_SIGNAL_RECOGNITION);
 	
 	if (optionsOneWindow) {		
 		namedWindow(WINDOW_NAME_OPTIONS, CV_WINDOW_NORMAL);
 		resizeWindow(WINDOW_NAME_OPTIONS, WINDOW_OPTIONS_WIDTH - WINDOW_FRAME_THICKNESS * 2, WINDOW_OPTIONS_HIGHT);
 		moveWindow(WINDOW_NAME_OPTIONS, screenWidth - WINDOW_OPTIONS_WIDTH, 0);
 	} else {						
-		addHighGUITrackBarWindow(WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS, 3, 0, 0);
-		addHighGUITrackBarWindow(WINDOW_NAME_BILATERAL_FILTER_OPTIONS, 3, 3, 1);
+		addHighGUITrackBarWindow(WINDOW_NAME_BILATERAL_FILTER_OPTIONS, 3, 0, 0);
+		addHighGUITrackBarWindow(WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS, 3, 3, 1);		
 		addHighGUITrackBarWindow(WINDOW_NAME_CONTRAST_AND_BRIGHTNESS_OPTIONS, 2, 6, 2);
 		addHighGUITrackBarWindow(WINDOW_NAME_COLOR_SEGMENTATION_OPTIONS, 6, 8, 3);
-	}
-		
-	cv::createTrackbar(TRACK_BAR_NAME_CLAHE_CLIP, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS), &claehClipLimit, 100, updateImageAnalysis, (void*)this);
-	cv::createTrackbar(TRACK_BAR_NAME_CLAHE_TILE_X, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS), &claehTileXSize, 20, updateImageAnalysis, (void*)this);
-	cv::createTrackbar(TRACK_BAR_NAME_CLAHE_TILE_Y, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS), &claehTileYSize, 20, updateImageAnalysis, (void*)this);
+		/*addHighGUITrackBarWindow(WINDOW_NAME_SIGNAL_CANNY_OPTIONS, 3, 14, 4);
+		addHighGUITrackBarWindow(WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS, 6, 17, 5);*/
+		addHighGUITrackBarWindow(WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS, 6, 14, 4);
+	}	
 	
 	cv::createTrackbar(TRACK_BAR_NAME_BI_FILTER_DIST, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_BILATERAL_FILTER_OPTIONS), &bilateralFilterDistance, 100, updateImageAnalysis, (void*)this);
 	cv::createTrackbar(TRACK_BAR_NAME_BI_FILTER_COLOR_SIG, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_BILATERAL_FILTER_OPTIONS), &bilateralFilterSigmaColor, 200, updateImageAnalysis, (void*)this);
 	cv::createTrackbar(TRACK_BAR_NAME_BI_FILTER_SPACE_SIG, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_BILATERAL_FILTER_OPTIONS), &bilateralFilterSigmaSpace, 200, updateImageAnalysis, (void*)this);
-		
+	
+	cv::createTrackbar(TRACK_BAR_NAME_CLAHE_CLIP, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS), &claehClipLimit, 100, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_CLAHE_TILE_X, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS), &claehTileXSize, 20, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_CLAHE_TILE_Y, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS), &claehTileYSize, 20, updateImageAnalysis, (void*)this);
+
 	cv::createTrackbar(TRACK_BAR_NAME_CONTRAST, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_CONTRAST_AND_BRIGHTNESS_OPTIONS), &contrast, 100, updateImageAnalysis, (void*)this);
 	cv::createTrackbar(TRACK_BAR_NAME_BRIGHTNESS, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_CONTRAST_AND_BRIGHTNESS_OPTIONS), &brightness, 1000, updateImageAnalysis, (void*)this);
 		
@@ -276,9 +430,18 @@ void ImageAnalysis::setupResultsWindows(bool optionsOneWindow) {
 	cv::createTrackbar(TRACK_BAR_NAME_COLOR_SEG_MAX_SAT, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_COLOR_SEGMENTATION_OPTIONS), &colorSegmentationUpperSaturation, 255, updateImageAnalysis, (void*)this);
 	cv::createTrackbar(TRACK_BAR_NAME_COLOR_SEG_MIN_VAL, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_COLOR_SEGMENTATION_OPTIONS), &colorSegmentationLowerValue, 255, updateImageAnalysis, (void*)this);
 	cv::createTrackbar(TRACK_BAR_NAME_COLOR_SEG_MAX_VAL, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_COLOR_SEGMENTATION_OPTIONS), &colorSegmentationUpperValue, 255, updateImageAnalysis, (void*)this);
+
+	/*cv::createTrackbar(TRACK_BAR_NAME_CANNY_LOWER_HYSTERESIS_THRESHOLD, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_CANNY_OPTIONS), &cannyLowerHysteresisThreshold, 100, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_CANNY_HIGHER_HYSTERESIS_THRESHOLD, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_CANNY_OPTIONS), &cannyHigherHysteresisThreshold, 300, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_CANNY_KERNEL_SIZE_SOBEL, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_CANNY_OPTIONS), &cannySobelOperatorKernelSize, 7, updateImageAnalysis, (void*)this);*/
+
+	cv::createTrackbar(TRACK_BAR_NAME_HOUGH_CIRCLES_DP, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS), &houghCirclesDP, 10, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_HOUGH_CIRCLES_MIN_DIST_CENTERS, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS), &houghCirclesMinDistanceCenters, 100, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_HOUGH_CIRCLES_CANNY_HIGHER_THRESHOLD, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS), &houghCirclesCannyHigherThreshold, 500, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_HOUGH_CIRCLES_ACCUMULATOR_THRESHOLD, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS), &houghCirclesAccumulatorThreshold, 250, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_HOUGH_CIRCLES_MIN_RADIUS, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS), &houghCirclesMinRadius, 100, updateImageAnalysis, (void*)this);
+	cv::createTrackbar(TRACK_BAR_NAME_HOUGH_CIRCLES_MAX_RADIUS, (optionsOneWindow? WINDOW_NAME_OPTIONS : WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS), &houghCirclesMaxRadius, 100, updateImageAnalysis, (void*)this);
 }
-
-
 
 
 void ImageAnalysis::addHighGUIWindow(int column, int row, string windowName, int numberColumns, int numberRows) {
@@ -318,6 +481,7 @@ void ImageAnalysis::addHighGUIWindow(int column, int row, string windowName, int
 	moveWindow(windowName, x, y);
 }
 
+
 void ImageAnalysis::addHighGUITrackBarWindow(string windowName, int numberTrackBars, int cumulativeTrackBarPosition, int trackBarWindowNumber) {
 	namedWindow(windowName, CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_EXPANDED);
 
@@ -325,7 +489,7 @@ void ImageAnalysis::addHighGUITrackBarWindow(string windowName, int numberTrackB
 	int height = numberTrackBars * WINDOW_OPTIONS_TRACKBAR_HEIGHT;
 	resizeWindow(windowName, width, height);
 
-	int heightPos = (WINDOW_FRAME_THICKNESS + WINDOW_HEADER_HEIGHT) * trackBarWindowNumber + WINDOW_OPTIONS_TRACKBAR_HEIGHT * cumulativeTrackBarPosition;
+	int heightPos = (WINDOW_HEADER_HEIGHT + WINDOW_FRAME_THICKNESS) * trackBarWindowNumber + WINDOW_OPTIONS_TRACKBAR_HEIGHT * cumulativeTrackBarPosition;
 	moveWindow(windowName, screenWidth - WINDOW_OPTIONS_WIDTH, heightPos);
 }
 
--- Proj1-TrafficSignDetection/src/ImageAnalysis/ImageAnalysis.h
+++ Proj1-TrafficSignDetection/src/ImageAnalysis/ImageAnalysis.h
@@ -2,56 +2,74 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 using std::string;
 using std::vector;
+using std::map;
+using std::pair;
 using cv::Mat;
 using cv::Scalar;
-using cv::imread;
+using cv::Vec3f;
+using cv::Point;
 using cv::VideoCapture;
+using cv::imread;
 using cv::waitKey;
 using cv::imshow;
 using cv::namedWindow;
 using cv::moveWindow;
 using cv::resizeWindow;
+using cv::circle;
 
 
 #define WINDOW_NAME_MAIN "0. Original image"
 #define WINDOW_NAME_OPTIONS "Parameterization"
 #define WINDOW_NAME_BILATERAL_FILTER "1. Bilateral filter"
 #define WINDOW_NAME_BILATERAL_FILTER_OPTIONS "1.1. Bilateral filter options"
-#define WINDOW_NAME_HISTOGRAM_EQUALIZATION "2a. Global histogram equalization (not used)"
-#define WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE "2b. Histogram equalization CLAHE"
+#define WINDOW_NAME_HISTOGRAM_EQUALIZATION "2. Global histogram equalization (not used)"
+#define WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE "2. Histogram equalization CLAHE"
 #define WINDOW_NAME_HISTOGRAM_EQUALIZATION_CLAHE_OPTIONS "2.1. Histogram equalization CLAHE options"
 #define WINDOW_NAME_CONTRAST_AND_BRIGHTNESS "3. Contrast and brightness"
 #define WINDOW_NAME_CONTRAST_AND_BRIGHTNESS_OPTIONS "3.1. Contrast and brightness options"
 #define WINDOW_NAME_COLOR_SEGMENTATION "4. Color segmentation"
 #define WINDOW_NAME_COLOR_SEGMENTATION_OPTIONS "4.1. Color segmentation configuration"
-#define WINDOW_NAME_SIGNAL_RECOGNITION "5. Signal recognition"
+#define WINDOW_NAME_SIGNAL_CANNY "5. Canny edge detector"
+#define WINDOW_NAME_SIGNAL_CANNY_OPTIONS "5.1. Canny edge detector options"
+#define WINDOW_NAME_SIGNAL_RECOGNITION "6. Signal recognition"
+#define WINDOW_NAME_SIGNAL_RECOGNITION_OPTIONS "6.1. Signal recognition options"
 
-#define TRACK_BAR_NAME_CLAHE_CLIP "Clip"
-#define TRACK_BAR_NAME_CLAHE_TILE_X "Tile X"
-#define TRACK_BAR_NAME_CLAHE_TILE_Y "Tile Y"
-#define TRACK_BAR_NAME_BI_FILTER_DIST "Dist"
-#define TRACK_BAR_NAME_BI_FILTER_COLOR_SIG "Color Sig"
-#define TRACK_BAR_NAME_BI_FILTER_SPACE_SIG "Space Sig"
-#define TRACK_BAR_NAME_CONTRAST "Contr * 10"
-#define TRACK_BAR_NAME_BRIGHTNESS "Brigh * 10"
-#define TRACK_BAR_NAME_COLOR_SEG_MIN_HUE "Min Hue"
-#define TRACK_BAR_NAME_COLOR_SEG_MAX_HUE "Max Hue"
-#define TRACK_BAR_NAME_COLOR_SEG_MIN_SAT "Min Sat"
-#define TRACK_BAR_NAME_COLOR_SEG_MAX_SAT "Max Sat"
-#define TRACK_BAR_NAME_COLOR_SEG_MIN_VAL "Min Val"
-#define TRACK_BAR_NAME_COLOR_SEG_MAX_VAL "Max Val"
+#define TRACK_BAR_NAME_BI_FILTER_DIST "1Dist"
+#define TRACK_BAR_NAME_BI_FILTER_COLOR_SIG "1Color Sig"
+#define TRACK_BAR_NAME_BI_FILTER_SPACE_SIG "1Space Sig"
+#define TRACK_BAR_NAME_CLAHE_CLIP "2Clip"
+#define TRACK_BAR_NAME_CLAHE_TILE_X "2Tile X"
+#define TRACK_BAR_NAME_CLAHE_TILE_Y "2Tile Y"
+#define TRACK_BAR_NAME_CONTRAST "3Contr*10"
+#define TRACK_BAR_NAME_BRIGHTNESS "3Brigh*10"
+#define TRACK_BAR_NAME_COLOR_SEG_MIN_HUE "4Min Hue"
+#define TRACK_BAR_NAME_COLOR_SEG_MAX_HUE "4Max Hue"
+#define TRACK_BAR_NAME_COLOR_SEG_MIN_SAT "4Min Sat"
+#define TRACK_BAR_NAME_COLOR_SEG_MAX_SAT "4Max Sat"
+#define TRACK_BAR_NAME_COLOR_SEG_MIN_VAL "4Min Val"
+#define TRACK_BAR_NAME_COLOR_SEG_MAX_VAL "4Max Val"
+//#define TRACK_BAR_NAME_CANNY_LOWER_HYSTERESIS_THRESHOLD "5CLowHyst"
+//#define TRACK_BAR_NAME_CANNY_HIGHER_HYSTERESIS_THRESHOLD "5CHighHyst"
+//#define TRACK_BAR_NAME_CANNY_KERNEL_SIZE_SOBEL "5CApperSbl"
+#define TRACK_BAR_NAME_HOUGH_CIRCLES_DP "6Hough DP"
+#define TRACK_BAR_NAME_HOUGH_CIRCLES_MIN_DIST_CENTERS "6MinDCntr%"
+#define TRACK_BAR_NAME_HOUGH_CIRCLES_CANNY_HIGHER_THRESHOLD "6CnyHiThrs"
+#define TRACK_BAR_NAME_HOUGH_CIRCLES_ACCUMULATOR_THRESHOLD "6AccumThrs"
+#define TRACK_BAR_NAME_HOUGH_CIRCLES_MIN_RADIUS "6MinRadiu%"
+#define TRACK_BAR_NAME_HOUGH_CIRCLES_MAX_RADIUS "6MaxRadiu%"
 
 #define	WINDOW_HEADER_HEIGHT 32
 #define WINDOW_FRAME_THICKNESS 8
 #define WINDOW_OPTIONS_WIDTH 350
-#define WINDOW_OPTIONS_HIGHT 666
-#define WINDOW_OPTIONS_TRACKBAR_HEIGHT 45
+#define WINDOW_OPTIONS_HIGHT 935
+#define WINDOW_OPTIONS_TRACKBAR_HEIGHT 44
 #define ESC_KEYCODE 27
 
 
@@ -66,8 +84,13 @@ class ImageAnalysis {
 		void preprocessImage(Mat& image, bool useCVHighGUI = true);
 		void histogramEqualization(Mat& image, bool use_CLAHE = true, bool useCVHighGUI = true);		
 
-		void segmentImage(Mat& image, bool useCVHighGUI = true);
-		void recognizeTrafficSigns(Mat& image, Mat colorSegmentedImage, bool useCVHighGUI = true);
+		Mat segmentImageByColor(Mat& image, bool useCVHighGUI = true);
+		vector<Vec3f> recognizeTrafficSignsCircles(Mat& colorSegmentedImage, Mat& image, bool useCVHighGUI = true);
+		vector<Vec3f> filterRecognizedTrafficSignCircles(const vector<Vec3f>& houghCircles);
+		void flatClustersByMedian(vector< vector<Vec3f> > &houghCirclesClusters, vector<Vec3f> &houghCirclesFiltered);
+		void flatClustersByMaxRadius(vector< vector<Vec3f> > &houghCirclesClusters, vector<Vec3f> &houghCirclesFiltered);
+
+		bool aggregateCircleIntoClusters(vector< vector<Vec3f> >& houghCirclesClusters, const Vec3f& centerToAdd);
 
 		bool updateImage();
 		
@@ -112,4 +135,15 @@ class ImageAnalysis {
 		int colorSegmentationUpperSaturation;
 		int colorSegmentationLowerValue;				
 		int colorSegmentationUpperValue;
+
+		int cannyLowerHysteresisThreshold;
+		int cannyHigherHysteresisThreshold;
+		int cannySobelOperatorKernelSize;
+
+		int houghCirclesDP;
+		int houghCirclesMinDistanceCenters;
+		int houghCirclesCannyHigherThreshold;
+		int houghCirclesAccumulatorThreshold;
+		int houghCirclesMinRadius;
+		int houghCirclesMaxRadius;
 };
